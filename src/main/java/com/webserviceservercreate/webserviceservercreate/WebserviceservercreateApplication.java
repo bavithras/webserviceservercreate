16ca02f1c442e89b0a46657fe526a749dbc98fa0
bavithras <bavi.rane@gmail.com>
2019-03-04 08:43:31 +0530

    first commit

---
+++ src/main/java/com/webserviceservercreate/webserviceservercreate/WebserviceservercreateApplication.java
@@ -0,0 +1,13 @@
+package com.webserviceservercreate.webserviceservercreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceservercreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceservercreateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webserviceservercreate/webserviceservercreate/WebserviceservercreateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webserviceservercreate.webserviceservercreate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebserviceservercreateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
